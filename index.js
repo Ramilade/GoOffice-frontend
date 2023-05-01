d580f601daacae086cf074ec18711acadcc7180a
Linding3r <thomasjuullindinger@yahoo.dk>
2023-05-01 14:29:32 +0200

    added main content div

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    // Set the initial position of the hamburger icon
+    $('.hamburger').css('left', '90px');
+
     // Toggle Sign In and Sign Out buttons
     $('.sign-btn').on('click', function(event) {
         event.preventDefault();
@@ -7,12 +10,19 @@ $(document).ready(function() {
 
     // Toggle sidebar collapse
     $('.hamburger').on('click', function() {
-        $('.sidebar').toggleClass('sidebar-collapsed');
-        $('.logo-expanded, .logo-collapsed').toggle(); // Toggle logo visibility
-        if ($('.sidebar').hasClass('sidebar-collapsed')) {
-            $('.hamburger').css('left', '90px'); // Position the hamburger next to the collapsed sidebar
+        if ($(window).width() > 768) {
+            $('.sidebar').toggleClass('sidebar-collapsed');
+            $('.logo-expanded, .logo-collapsed').toggle(); // Toggle logo visibility
+            if ($('.sidebar').hasClass('sidebar-collapsed')) {
+             $('.hamburger').css('left', '90px'); // Position the hamburger next to the collapsed sidebar
+         } else {
+             $('.hamburger').css('left', '220px'); // Position the hamburger next to the expanded sidebar
+         }
+            $('.main-content').toggleClass('sidebar-collapsed');
         } else {
-            $('.hamburger').css('left', '220px'); // Position the hamburger next to the expanded sidebar
+            // Toggle menu visibility for mobile view
+            $('.sidebar').toggleClass('mobile-active');
+            $('.sidebar a:not(.sign-btn)').toggle();
         }
     });
 });
