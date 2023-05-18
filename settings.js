397bdf08ae6306ad93cc0fe943dd96bd78bb44e4
Linding3r <thomasjuullindinger@yahoo.dk>
2023-05-18 09:34:32 +0200

    vacation added

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://goffice.azurewebsites.net"
-// export const API_URL = "http://localhost:8080/api/"
+//export const API_URL = "http://localhost:8080"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
 
