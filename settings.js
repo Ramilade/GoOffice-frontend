c47e55f3850e6db93823e12a081a5334b3dcbb5e
Ramilade <rami0904@stud.kea.dk>
2023-05-22 21:01:41 +0200

    url fix

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://jftr-gooffice.azurewebsites.net"
+export const API_URL = "https://goofficerami.azurewebsites.net"
 //export const API_URL = "http://localhost:8080"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
